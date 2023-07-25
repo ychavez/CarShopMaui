bf23d184ded73ecb34177ff7b3d68c14f4de2dbc
Yael Chavez <you400@gmail.com>
2023-07-24 20:54:54 -0600

    rest service

---
+++ Context/RestService.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using CarShopMaui.Models;
+using System.Text.Json;
+
+namespace CarShopMaui.Context
+{
+    public class RestService
+    {
+        HttpClient _httpClient;
+        Uri _url;
+
+        public RestService()
+        {
+            _url = new Uri("URL_BASE");
+            _httpClient = new();
+            _httpClient.BaseAddress = _url;
+        }
+
+        public async Task<List<Car>> GetCars()
+        {
+            var response = await _httpClient.GetAsync("URL_GET");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<List<Car>>(content);
+            }
+
+            throw new Exception("Error al tratar de obtener la informacion");
+        }
+
+        public async Task SetCar(Car car)
+        {
+            var json = JsonSerializer.Serialize(car);
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("URL_Post", data);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Error al tratar de enviar la informacion");
+
+        }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace CarShopMaui.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Year { get; set; }
+        public string PhotoURL { get; set; }
+
+        public double? Lat  { get; set; }
+        public double? Lon { get; set; }
+    }
+}
